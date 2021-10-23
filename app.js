5d945cce809c220728db2083288d75fd70e9db6c
massoudasadi6tk5s <yuanpaodengdu4689@163.com>
2021-10-23 10:22:02 +0800

    1.规范增加2.app.js部分注释3.welcome页面样式修改和js部分注释

--- app.js
+++ app.js
@@ -4,34 +4,34 @@ const ajax = require('utils/ajax.js')
 App({
   onLaunch: function () {
 
-    ajax.HTTP.get(ajax.API.testTwo, null, function(res){}, 'json')
+    // ajax.HTTP.get(ajax.API.testTwo, null, function(res){}, 'json')
 
 
-    if(wx.getStorageSync('userInfo')){
-      return
-    }
+    // if(wx.getStorageSync('userInfo')){
+    //   return
+    // }
 
-    // 发送code 到后端，验证用户有没有授权  没有授权就不做任何授权； 授权就返回用户信息缓存起来
-    wx.login({
-      success(res) {
+    // // 发送code 到后端，验证用户有没有授权  没有授权就不做任何授权； 授权就返回用户信息缓存起来
+    // wx.login({
+    //   success(res) {
 
-        console.log(res.code)
+    //     console.log(res.code)
 
-        let params = {
-          code: res.code
-        }
+    //     let params = {
+    //       code: res.code
+    //     }
 
-        ajax.HTTP.post(ajax.API.detectionUserAuthorization, params, function (res) {
-          console.log(res)
+    //     ajax.HTTP.post(ajax.API.detectionUserAuthorization, params, function (res) {
+    //       console.log(res)
           
-          if (!res.data.result) {
-            return
-          }
-          wx.setStorageSync('userInfo', res.data.result)
-        }, 'json');
+    //       if (!res.data.result) {
+    //         return
+    //       }
+    //       wx.setStorageSync('userInfo', res.data.result)
+    //     }, 'json');
 
-      }
-    })
+    //   }
+    // })
 
 
   },
