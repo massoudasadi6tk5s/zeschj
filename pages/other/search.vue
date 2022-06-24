8fbde13c45da47a15cbf01b2ba19067d5f1a24bd
massoudasadi6tk5s <yuanpaodengdu4689@163.com>
2022-06-25 01:30:14 +0800

    搜索界面(做了一丢丢)
    今天坐错公交车，耽误了好久，天还下雨😥

---
+++ pages/other/search.vue
@@ -0,0 +1,69 @@
+<template>
+	<view class="body">
+		<view class="search-title">Search</view>
+		<view class="search-input-container">
+			<view class="search-input">
+				<image :src="searchIcon"></image>
+				<input class="" />
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				searchIcon: 'https://weiju1.oss-cn-shenzhen.aliyuncs.com/home/search.png',
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	page {
+		height: 100%;
+		width: 100%;
+		background-color: $bg-gray-dark;
+	}
+
+	.body {
+		height: 100%;
+		width: 100%;
+		
+		.search-title{
+			margin-top: 212rpx;
+			margin-left: 30rpx;
+			  color: $font-ffffff;
+			  font-family: "Avenir-Heavy";
+			  font-size: 34px;
+			  font-weight: 400;
+		}
+		
+		.search-input-container{
+			
+			padding: 0 30rpx;
+			
+			.search-input{
+				display: flex;
+				align-items: center;
+				margin-top: 40rpx;
+				height: 72rpx;
+				border-radius: 10px;
+				background: rgba(142,142,147,0.12);
+				
+				image {
+					margin-left: 30rpx;
+					width: 48rpx;
+					height: 48rpx;
+				}
+			}
+			
+			
+		}
+		
+	}
+</style>
